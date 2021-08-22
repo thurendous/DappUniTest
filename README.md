0ce4cf0d8bf36d7486f2c92e0fcc54a59b854e91
thurendous <thurendous2@gmail.com>
2021-08-22 19:03:57 +0900

    daily grind

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # DappUniTest
 Building some project on the way of learning about Dapps. 
 # CreateERC20.sol: create a ERC20 token and release it on Uniswap. 
+
---
+++ mocha-chai/math.test.js
@@ -0,0 +1,12 @@
+const { add } = require("./math");
+
+const assert = require("chai").assert;
+
+describe("add()", () => {
+  it("should add 2 numbers", () => {
+    const a = 1;
+    const b = 1;
+    const actual = add(a, b);
+    assert.equal(actual, 2);
+  });
+}); // comes from mocha
